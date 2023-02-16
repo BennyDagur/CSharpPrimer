051e546a2f25cbfc58c8e60c3089d0dd4134942e
BennyDagur <bdg2@nemandi.ntv.is>
2023-02-16 17:10:24 +0000

    Add project files.

---
+++ CSharpPrimer/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Input numbers and I'll print out both the smallest and largest numbers");
+
+        List<int> numbers = new List<int>();
+        int listNumbers;
+        bool inputCheck = true;
+
+        while (inputCheck) { 
+            string input = Console.ReadLine();
+            if (input.Trim() == "")
+            {
+                inputCheck = false;
+            } else if (!int.TryParse(input, out listNumbers)) {
+                Console.WriteLine("That's not a number, try again");
+            } else
+            {
+                listNumbers = int.Parse(input);
+                numbers.Add(listNumbers);
+            }
+        }
+
+        numbers.Sort();
+
+        Console.WriteLine($"{numbers.First()} Is the smallest number and {numbers.Last()} Is the largest");
+    }
+}
---
+++ WeatherData/Program.cs
@@ -0,0 +1,179 @@
+﻿class Program
+{
+    class WeatherData
+    {
+        int temperature;
+        int humidity;
+        string scale;
+
+        static void Convert(int minValue, int maxValue, string scaleValue)
+        {
+            var wi = new WeatherData();
+            wi.scale = scaleValue;
+            string yOrN;
+
+            bool tempCheck = true;
+            bool humidCheck = true;
+            bool conversionCheck = true;
+
+            while (tempCheck)
+            {
+                Console.WriteLine($"Input temperature at or between {minValue} and {maxValue}");
+                wi.temperature = int.Parse(Console.ReadLine());
+                if (wi.temperature >= minValue && wi.temperature <= maxValue)
+                {
+                    while (humidCheck)
+                    {
+                        Console.WriteLine("Input humidity at or between 0% and 100%");
+                        wi.humidity = int.Parse(Console.ReadLine());
+                        if (wi.humidity >= 0 && wi.humidity <= 100)
+                        {
+                            humidCheck = false;
+                            tempCheck = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Humidity must be at or between 0% and 100%");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"You must have made a mistake, because only values at or between {minValue} and {maxValue} for this temperature system are valid");
+                }
+            }
+
+            Console.WriteLine($"It Is {wi.temperature + wi.scale} outside with a humidity of {wi.humidity}%");
+
+            while (conversionCheck)
+            {
+                Console.WriteLine($"Would you like to change from {wi.scale}? Enter Y for yes or N for no");
+                yOrN = Console.ReadLine().ToLower();
+                    if(yOrN == "y")
+                {
+                    if(wi.scale == "C")
+                    {
+                        wi.scale = "F";
+                        wi.temperature = ((wi.temperature * 9/5) + 32);
+                        Console.WriteLine($"It Is {wi.temperature + wi.scale} outside with a humidity of {wi.humidity}%");
+                    } else if (wi.scale == "F")
+                    {
+                        wi.scale = "C";
+                        wi.temperature = ((wi.temperature - 32) * 5/9);
+                        Console.WriteLine($"It Is {wi.temperature + wi.scale} outside with a humidity of {wi.humidity}%");
+                    }
+                } else if(yOrN == "n")
+                {
+                    conversionCheck = false;
+                } else
+                {
+                    Console.WriteLine("Repeat, enter Y for yes or N for no");
+                }
+            }
+        }
+
+        static void Main()
+            {
+                Console.WriteLine("Either enter 'C' for Celsius or 'F' for Fahrenheit");
+
+                var wi = new WeatherData();
+
+                bool check = true;
+
+                while (check)
+                {
+                    wi.scale = Console.ReadLine().ToUpper();
+                    if (wi.scale == "C" || wi.scale == "F")
+                    {
+                        check = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("I said either C for Celsius or F for Fahrenheit");
+                    }
+                }
+                if (wi.scale == "C")
+                {
+                  Convert(-60, 60, wi.scale);
+                }
+                else if (wi.scale == "F")
+                {
+                  Convert(-76, 140, wi.scale);
+                }
+        }
+
+
+            /*
+             * I thought this wouldn't work not realizing I could fit In the logic
+             
+            static void Convert(int minValue, int maxValue, string scaleValue)
+            {
+                var wi = new WeatherData();
+                wi.scale = scaleValue;
+
+                bool tempCheck = true;
+                bool humidCheck = true;
+
+                    while (tempCheck)
+                    {
+                        Console.WriteLine($"Input temperature at or between {minValue} and {maxValue}");
+                        wi.temperature = int.Parse(Console.ReadLine());
+                        if (wi.temperature >= minValue && wi.temperature <= maxValue)
+                        {
+                            while (humidCheck)
+                            {
+                                Console.WriteLine("Input humidity at or between 0% and 100%");
+                                wi.humidity = int.Parse(Console.ReadLine());
+                                if (wi.humidity >= 0 && wi.humidity <= 100)
+                                {
+                                    humidCheck = false;
+                                    tempCheck = false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Humidity must be at or between 0% and 100%");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You must have made a mistake, because only values at or between {minValue} and {maxValue} for this temperature system are valid");
+                        }
+                    }
+
+                Console.WriteLine($"It Is {wi.temperature + wi.scale} outside with a humidity of {wi.humidity}%");
+            }
+
+            static void Main()
+            {
+                Console.WriteLine("Either enter 'C' for Celsius or 'F' for Fahrenheit");
+
+                var wi = new WeatherData();
+
+                bool check = true;
+
+                while (check)
+                {
+                    wi.scale = Console.ReadLine().ToUpper();
+                    if (wi.scale == "C" || wi.scale == "F")
+                    {
+                        check = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("I said either C for Celsius or F for Fahrenheit");
+                    }
+                }
+
+                if(wi.scale == "C")
+                {
+                    Convert(-60, 60, wi.scale);
+                } else if (wi.scale == "F")
+                {
+                    Convert(-76, 140, wi.scale);
+                }
+            }
+            */
+        }
+    }
+        
